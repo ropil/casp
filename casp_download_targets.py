d0b0d122b08f55d392d7804adb88af40b248ba02
Robert <robert.pilstal@liu.se>
2017-06-30 10:53:30 +0200

    Script to download CASP* targets
    In working condition; but will not integrity check downloads; partial
    downloads will trickle down the pipe as it is now coded.

--- casp_download_targets.py
+++ casp_download_targets.py
@@ -2,12 +2,12 @@
 from os import listdir
 from os.path import isfile, join
 from re import compile
-from urllib.request import urlopen
+from urllib.request import urlopen, urlretrieve
 from bs4 import BeautifulSoup
 
 
 # Library functions
-def find_targets_new(url, target_regex, targets_downloaded=None):
+def find_targets_new(url, target_regex, targets_downloaded=None, verbose=False):
     # Initialize a new targets downloaded if none is provided
     if targets_downloaded is None:
         targets_downloaded = {}
@@ -23,26 +23,31 @@ def find_targets_new(url, target_regex, targets_downloaded=None):
 
     for target in targets:
         if not target.string in targets_downloaded:
-            print(target.string)
+            if verbose:
+                print(target.string)
             new_targets[target.string] = url + "/" + target.string
 
     return new_targets
 
 
-def find_targets_downloaded(directory, target_regex):
+def find_targets_downloaded(directory, target_regex, verbose=False):
     targets_downloaded = {}
     for target in listdir(directory):
         target_path = join(directory, target)
         if isfile(join(directory, target)):
-            print("Found target " + target + " with path " + target_path)
+            if verbose:
+                print("Found target " + target + " with path " + target_path)
             targets_downloaded[target] = target_path
     return targets_downloaded
 
 
-def download_targets(new_targets, destination):
+def download_targets(new_targets, destination, verbose=False):
     # Print what is happening
     for target in new_targets:
-        print("\t".join(["Downloading", target, "from", new_targets[target]]))
+        download_path = destination + "/" + target
+        if verbose:
+            print("\t".join(["Downloading", target, "from", new_targets[target], "->", download_path ]))
+        urlretrieve(new_targets[target], download_path)
 
 
 # Main; for callable scripts
@@ -52,7 +57,7 @@ def main():
     parser = ArgumentParser(
         description="Download CASP12 targets.")
     parser.add_argument(
-        "-a", action="store_true", default=False, help="Prints nothing")
+        "-verbose", action="store_true", default=False, help="Be verbose, default=silent")
     parser.add_argument(
         "-url", nargs=1, default=["http://predictioncenter.org/download_area/CASP12/server_predictions/"],
         metavar="URL", help="CASP download area directory, " +
@@ -72,6 +77,7 @@ def main():
         directories = stdin
 
     # Set variables here
+    verbose = arguments.verbose
     target_regex = compile(arguments.regex[0])
     url = arguments.url[0]
     destination = arguments.dest[0]
@@ -83,17 +89,19 @@ def main():
         # find directory targets contents
         if isinstance(d, str):
             if destination is None:
-                print("Setting destination directory to " + d)
+                if verbose:
+                    print("Setting destination directory to " + d)
                 destination = d
-            print("Parse content in directory " + d)
+            if verbose:
+                print("Parse content in directory " + d)
             # and join all found in different directories; avoiding duplicates
-            targets_downloaded = {**targets_downloaded, **find_targets_downloaded(d, target_regex)}
+            targets_downloaded = {**targets_downloaded, **find_targets_downloaded(d, target_regex, verbose=verbose)}
 
     # Get all new target urls
-    new_targets = find_targets_new(url, target_regex, targets_downloaded)
+    new_targets = find_targets_new(url, target_regex, targets_downloaded, verbose=verbose)
 
     # Download the targets
-    download_targets(new_targets, destination)
+    download_targets(new_targets, destination, verbose=verbose)
 
 if __name__ == '__main__':
     main()
